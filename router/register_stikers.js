4b40888bd043fb123ecf60d8b68032d3b6fbe493
MOdevco <modevco05@gmail.com>
2024-04-16 16:39:20 +0500

    qwer

--- router/register_stikers.js
+++ router/register_stikers.js
@@ -41,10 +41,10 @@ router.post('/', async (req, res) => {
         status: true,
         massage: "Tabriklaymiz siz konkurs ishtirokchisiga aylandingiz"
     })
-   
+
     const formdata = new FormData();
     formdata.append("mobile_phone", req.body.phoneNumber);
-    formdata.append("message", `${req.body.firstName} ${req.body.lastName}, siz ishtirokchiga aylandingiz. OMAD TILAYMIZ!`);
+    formdata.append("message", ` NAZOKAT LADY SHOP!  ${req.body.firstName} ${req.body.lastName}! Siz LADY SHOU o'yin ishtirokchisiga aylandingiz. OMAD TILAYMIZ. Qo'shimcha ma'lumot uchun: https://t.me/bizda_hamm_75mingnazokat`);
     formdata.append("from", "4546");
 
     const requestOptions = {
